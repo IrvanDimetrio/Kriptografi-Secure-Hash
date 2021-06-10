dd71ccdb85837020a54d375a067388db9a31bff0
M. Irvan Dimetrio <52452132+IrvanDimetrio@users.noreply.github.com>
2021-06-10 11:39:49 +0700

    Add files via upload

---
+++ KriptografiSecureHash/src/main/java/com/kriptorio/kriptografisecurehash/App.java
@@ -0,0 +1,28 @@
+package com.kriptorio.kriptografisecurehash;
+// Dibuat Oleh : M.Irvan Dimetrio(18360018)
+
+import org.apache.commons.codec.binary.Base64;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Aplikasi Secure Hash dengan algoritma hash Dapat dirubah algoritma hash pada
+ * argumen di object SecureHash misal: SHA512, SHA256, MD5, dlsb
+ */
+public class App {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Masukkan plainText : ");
+        String str = bReader.readLine();
+        SecureHash secureHash = new SecureHash("SHA512");
+        byte[] hashResult = secureHash.doHash(str);
+        String hashResultInHexString = Convert.byteArrayToHexString(hashResult);
+        String hashResultInByteString = Convert.byteArrayToBytesString(hashResult);
+        System.out.println("Digest dalam hexa : " + hashResultInHexString);
+        System.out.println("Digest dalam bytes : " + hashResultInByteString);
+        System.out.println("Digest dalam ascii : " + Base64.encodeBase64String(hashResult));
+    }
+}
